8f83f9f97acc47b94d6def5a7f398af044b52675
SVignesh2004 <sivarajvignesh2004@gmail.com>
2025-02-09 09:19:27 +0530

    backend first push

---
+++ pet-shop-backend/src/main/java/com/example/pet_shop_backend/PetShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pet_shop_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetShopBackendApplication.class, args);
+	}
+
+}
---
+++ pet-shop-backend/src/test/java/com/example/pet_shop_backend/PetShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pet_shop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
